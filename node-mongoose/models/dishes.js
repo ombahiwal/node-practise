52eb64266c4651c8debd5e4171d2302d2aeeb1a8
ombahiwal@gmail.com <ombahiwal@gmail.com>
2020-08-05 01:40:41 +0530

    Mongoose Schemas and Sub-Schemas

--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+},{timestamps: true});
+
 const dishSchema = new Schema({
     name: {
         type: String,
@@ -11,7 +28,8 @@ const dishSchema = new Schema({
         type: String,
         require: true,
         unique: false
-    }
+    },
+    comments:[commentSchema]
 },{
     timestamps: true
 });
